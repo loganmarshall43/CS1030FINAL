0d43d0781284fad9dbd77eb2eb54f3d94f1c7a84
Logan Marshall <loganmarshall43@gmail.com>
2025-08-09 21:41:09 -0600

    button try

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 document.getElementById('learn-more').addEventListener('click', () => {
     document.getElementById('site-overview').scrollIntoView({ behavior: 'smooth' });
 });
+
+document.getElementById('learn-more').addEventListener('click', () => {
+    document.getElementById('species-info').scrollIntoView({ behavior: 'smooth' });
+});
